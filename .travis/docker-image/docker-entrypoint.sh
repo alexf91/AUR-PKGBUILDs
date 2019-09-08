8ec97348be412444f21cfe8fb2d3bfa8907911f9
Alexander Fasching <fasching.a91@gmail.com>
2019-09-08 16:44:00 +0200

    Install AUR dependencies during testing

--- .travis/docker-image/docker-entrypoint.sh
+++ .travis/docker-image/docker-entrypoint.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
+#
+# Install package dependencies and build the package.
+# TODO: Maybe look for dependencies locally. This might be helpful during initial
+#       package development when nothing has been submitted to the AUR.
+#
 
 set -e
+
+if [[ $# != 1 ]]; then
+    echo "Invalid number of arguments"
+    exit 1
+fi
+
+MAKEPKG_FLAGS="--noconfirm --syncdeps --cleanbuild --install"
 
 # Copy the package we want to build to a separate directory to avoid cluttering
 # the base repo.
 cp -r /repo/$1 /tmp/build-pkg
 cd /tmp/build-pkg
-makepkg --noconfirm --syncdeps --cleanbuild --install
+
+# Load the PKGBUILD file
+source PKGBUILD
+
+# Install make dependencies and dependencies.
+yay -S --noconfirm --asdeps ${makedepends[@]} ${depends[@]}
+
+# Git is not a dependency by default, so we remove it and build the package.o
+# Syncdeps is enabled, so it will be reinstalled if necessary.
+sudo pacman --noconfirm -Rs git
+makepkg $MAKEPKG_FLAGS
