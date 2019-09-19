7de1ae84b47cf5bdb94836ef2a2fe1697c94c533
Alexander Fasching <fasching.a91@gmail.com>
2019-09-19 23:38:26 +0200

    Run namcap after tests
    Return value and results are not checked, but the output might be
    interesting.

--- .travis/docker-image/docker-entrypoint.sh
+++ .travis/docker-image/docker-entrypoint.sh
@@ -13,6 +13,7 @@ if [[ $# != 1 ]]; then
 fi
 
 MAKEPKG_FLAGS="--noconfirm --syncdeps --cleanbuild --install"
+export PKGEXT=.pkg.tar
 
 # Copy the package we want to build to a separate directory to avoid cluttering
 # the base repo.
@@ -28,4 +29,8 @@ yay -S --noconfirm --asdeps ${makedepends[@]} ${depends[@]}
 # Git is not a dependency by default, so we remove it and build the package.o
 # Syncdeps is enabled, so it will be reinstalled if necessary.
 sudo pacman --noconfirm -Rs git
-PKGEXT=.pkg.tar makepkg $MAKEPKG_FLAGS
+makepkg $MAKEPKG_FLAGS
+
+# Check the package with namcap
+sudo pacman --noconfirm -S namcap
+namcap PKGBUILD *"$PKGEXT"
